a7b4b35556d19dc721faa20b8244750ce3a6b085
Lobiritus <Lobiritus@DESKTOP-FB8M6RA>
2022-04-03 11:00:33 +0300

    Добавьте файлы проекта.

---
+++ Notes.Pesistance/DbInitializer.cs
@@ -0,0 +1,10 @@
+﻿namespace Notes.Pesistance
+{
+    public class DbInitializer
+    {
+        public static void Initialize(NotesDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
